80608470ae5943a05156f410df6494b4081a75ea
mysccasty <morcasersha@gmail.com>
2023-12-05 11:16:26 +0300

    w/o exept

--- ParCheb/ParCheb.cpp
+++ ParCheb/ParCheb.cpp
@@ -1,9 +1,37 @@
 ﻿#include "ParCheb.h"
+void generateMatr(double*& a0, double*& a1, double*& a2, double*& a3, double*& a4, double*& f, int nx, int ny) {
+    for (int i = 0; i < nx * ny; i++) {
+        a0[i] = 2.93333;
+        if (i == 0 | i == nx - 1 | i == nx * (ny - 1) | i == nx * ny - 1) {
+            f[i] = 0.133333;
+        }
+        else if ((i > 0 & i < nx - 1) | ((i + 1) % nx == 0) | (i % nx == 0) | (i > nx * (ny - 1) & i < nx * ny - 1)) {
+            f[i] = 0.133333;
+        }
+        else {
+            f[i] = 0;
+        }
+    }
+    for (int i = 0; i < nx * ny - 1; i++) {
+        if (((i + 1) % nx == 0) & (i != 0)) {
+            a1[i] = 0;
+            a2[i] = 0;
 
+        }
+        else {
+            a1[i] = -0.0666667;
+            a2[i] = -0.0666667;
+        }
+    }
+    for (int i = 0; i < nx * (ny - 1); i++) {
+        a3[i] = -0.0666667;
+        a4[i] = -0.0666667;
+    }
+}
 double norm(double* vector, int len) {
     int i;
     double sum = 0;
-#pragma omp parallel for private(i) reduction(+:sum)
+//#pragma omp parallel for private(i) reduction(+:sum)
     for (i = 0; i < len; i++) {
         sum += vector[i] * vector[i];
     }
@@ -14,10 +42,10 @@ double norm(double* vector, int len) {
 void nevyazka(double* a0, double* a1, double* a2, double* a3, double* a4, double* f, double* u, double*& R, int nx, int ny) {
 
     int i;
-#pragma omp parallel for shared(a0, a1, a2, a3, a4, f, u, R) private(i)
+//#pragma omp parallel for shared(a0, a1, a2, a3, a4, f, u, R) private(i)
     for (i = 0; i < nx * ny; i++) {
         if (i == 0) {
-            R[i] = f[i]-(a0[i] * u[i] + a1[i] * u[i + 1] + a3[i] * u[i + nx]);
+            R[i] = f[i] - (a0[i] * u[i] + a1[i] * u[i + 1] + a3[i] * u[i + nx]);
         }
         else if (i < nx) {
             R[i] = f[i] - (a2[i - 1] * u[i - 1] + a0[i] * u[i] + a1[i] * u[i + 1] + a3[i] * u[i + nx]);
@@ -36,28 +64,18 @@ void nevyazka(double* a0, double* a1, double* a2, double* a3, double* a4, double
 }
 //Решение СЛАУ
 void cheb(int nx, int ny, int N, double* a0, double* a1, double* a2, double* a3, double* a4,
-    double* f, double tau, ofstream& out) {
+    double* f, double* x, double tau) {
 
     double* r = (double*)malloc((nx * ny) * sizeof(double));
-    double* x = (double*)malloc((nx * ny) * sizeof(double));
-    double epsilon = 1e-10;
-    out << "Nx = " << nx << endl;
-    out << "Ny = " << ny << endl;
+    double epsilon = 1e-15;
     double fNorm;
     for (int i = 0; i < nx * ny; i++) {
         r[i] = f[i];
-        x[i] = 0;
     }
-
-
-    auto start = std::chrono::high_resolution_clock::now();
     fNorm = norm(f, nx * ny);
-    nevyazka(a0, a1, a2, a3, a4, f, x, r, nx, ny);
-    out << endl;
     for (int iter = 1; iter < N; iter++) {
 
-        if (norm(r, nx * ny) < epsilon * fNorm) {
-            out << iter << " iterations need to get " << epsilon << " error" << endl;
+        if (norm(r, nx * ny) < epsilon) {
             break;
         }
         for (int i = 0; i < nx * ny; i++) {
@@ -67,33 +85,20 @@ void cheb(int nx, int ny, int N, double* a0, double* a1, double* a2, double* a3,
 
 
     }
-    for (int i = 0; i < nx * ny; i++) {
-        //out << std::fixed;
-        //out << std::setprecision(15);
-        out << x[i] << "\t";
-    }
-    out << endl;
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double, std::milli> duration = end - start;
-    out << "Time: " << duration.count() << " ms" << endl;
-
 }
-void SLAE(double* s0, double* s1, double* s2, double* s3, double* s4, double* f, int nx, int ny) {
-    int N = 128;
+void SLAE(double* s0, double* s1, double* s2, double* s3, double* s4, double* x, double* f, int nx, int ny) {
+    int N = 1024;
 
     double* a0 = s0;
     double* a1 = s3;
     double* a2 = s1;
     double* a3 = s4;
     double* a4 = s2;
-
-    double* a = (double*)malloc((N) * sizeof(double));
-    double* b = (double*)malloc((N) * sizeof(double));
     double R = 0;
-
     double fNorm;
     double lam_max = -DBL_MAX;
     double lam_min = DBL_MAX;
+
     for (int i = 0; i < nx * ny; i++) {
         if (i == 0) {
             R = abs(a1[i]) + abs(a3[i]);
@@ -113,15 +118,54 @@ void SLAE(double* s0, double* s1, double* s2, double* s3, double* s4, double* f,
         if (lam_max < a0[i] + R) {
             lam_max = a0[i] + R;
         }
-        if (lam_min > a0[i]-R) {
-            lam_min = a0[i]-R;
+        if (lam_min > a0[i] - R) {
+            lam_min = a0[i] - R;
         }
     }
     double tau = 2 / (lam_min + lam_max);
     omp_set_dynamic(0);
     omp_set_num_threads(12);
+    cheb(nx, ny, N, a0, a1, a2, a3, a4, f, x, tau);
+}
+int main() {
+    int nx = 2, ny = 2;
+    double* a0 = (double*)malloc((nx * ny) * sizeof(double));
+    double* a1 = (double*)malloc((nx * ny-1) * sizeof(double));
+    double* a2 = (double*)malloc((nx * ny-1) * sizeof(double));
+    double* a3 = (double*)malloc((nx * (ny-1)) * sizeof(double));
+    double* a4 = (double*)malloc((nx * (ny-1)) * sizeof(double));
+    double* f = (double*)malloc((nx * ny) * sizeof(double));
+    generateMatr(a0, a1, a2, a3, a4, f, nx, ny);
+    double* x = (double*)malloc((nx * ny) * sizeof(double));
+    for (int i = 0; i < nx * ny; i++) {
+        x[i] = 0;
+    }
     std::ofstream result;
     result.open("result.txt");
-    cheb(nx, ny, N, a0, a1, a2, a3, a4, f, tau, result);
+    SLAE(a0, a2, a4, a1, a3, x, f, nx, ny);
+    for (int i = 0; i < nx * ny; i++) {
+        result << x[i] << endl;
+    }
     result.close();
+
+    
+    /*for (int i = 0; i < nx * ny; i++) {
+        if (i == 0) {
+            cout << a0[i] * x[i] + a1[i] * x[i + 1] + a3[i] * x[i + nx] << "\t";
+        }
+        else if (i < nx) {
+            cout << a2[i - 1] * x[i - 1] + a0[i] * x[i] + a1[i] * x[i + 1] + a3[i] * x[i + nx] << "\t";
+        }
+        else if (i == nx * ny - 1) {
+            cout << a4[i - nx] * x[i - nx] + a2[i - 1] * x[i - 1] + a0[i] * x[i] << "\t";
+        }
+        else if (i >= nx * (ny - 1)) {
+            cout << a4[i - nx] * x[i - nx] + a2[i - 1] * x[i - 1] + a0[i] * x[i] + a1[i] * x[i + 1] << "\t";
+
+        }
+        else {
+            cout << a4[i - nx] * x[i - nx] + a2[i - 1] * x[i - 1] + a0[i] * x[i] + a1[i] * x[i + 1] + a3[i] * x[i + nx] << "\t";
+        }
+    }*/
+    return 0;
 }
--- ParCheb/ParCheb.h
+++ ParCheb/ParCheb.h
@@ -11,8 +11,8 @@
 using namespace std;
 
 double norm(double* vector, int len);
-void nevyazka(double* a0, double* a1, double* a2, double* a3, double* a4, double* f, double* u, double*& R, int nx, int ny, double alpha);
-void cheb(int nx, int ny, int N, double* a0, double* a1, double* a2, double* a3, double* a4, double* f, double* beta, double* alpha, ofstream& out);
-void SLAU(double* s0, double* a1, double* a2, double* a3, double* a4, double* f, int nx, int ny);
+void nevyazka(double* a0, double* a1, double* a2, double* a3, double* a4, double* f, double* u, double*& R, int nx, int ny);
+void cheb(int nx, int ny, int N, double* a0, double* a1, double* a2, double* a3, double* a4, double* f, double* x, double tau);
+void SLAE(double* s0, double* s1, double* s2, double* s3, double* s4, double* x, double* f, int nx, int ny);
 
 #endif // PARCHEB_H
